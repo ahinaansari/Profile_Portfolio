b83d0409855fc25f8b30d4f0340b4ad0ec02c842
hina ansari <ahina4548@gmail.com>
2024-06-06 12:21:40 +0530

    Add New File

---
+++ Index.js
@@ -0,0 +1,16 @@
+let MenuBtn = document.getElementById("MenuBtn")
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector("body").classList.toggle('mobile-nav-action')
+    this.classList.toggle("fa-xmark")
+})
+
+
+let typed = new typed(".auto-input",{
+    strings: ['front-End Developer!', "Back-End Developer", "Freelancer" , " Social-Bloger" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay : 2000,
+    loop : true,
+})
+
